9000fdcc03c0b2415389e20d8015029b807df92a
wendecoder <wenonezra258@gmail.com>
2022-10-19 09:22:51 +0300

    done 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Returns copleted todos for a given employee ID."""
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "todos", params={"userId": id}).json()
+    users = requests.get(url + "users/{}".format(id)).json()
+
+    with open("{}.json".format(id), "w") as filejson:
+        json.dump({id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": users.get("username")
+            } for t in user]}, filejson)
