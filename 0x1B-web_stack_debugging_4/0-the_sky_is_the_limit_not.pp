96d7a867ca2a5222145abc7f249430e5645032a4
wendecoder <wenonezra258@gmail.com>
2022-11-10 09:59:40 +0300

    fixed the server

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# Increases the amount of traffic an Nginx server can handle
+# and then restart the server
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
